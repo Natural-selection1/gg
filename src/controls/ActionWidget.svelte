96516902769185acbd4a859062ee7ed51019fff8
Hegui Dai <natural_selection_@outlook.com>
2025-05-29 22:02:04 +0800

    使用标准css语法, 并修复样式错误

--- src/controls/ActionWidget.svelte
+++ src/controls/ActionWidget.svelte
@@ -30,6 +30,7 @@
         outline: none;
         margin: 0;
         background: var(--ctp-flamingo);
+        color: var(--ctp-base);
         border-width: 1px;
         border-radius: 3px;
         border-color: var(--ctp-overlay0);
@@ -43,35 +44,41 @@
         padding: 1px 6px;
     }
 
-    button:not(:disabled) {
-        &:hover {
-            background: var(--ctp-maroon);
-        }
-        &:focus-visible {
-            border-color: var(--ctp-lavender);
-            border-width: 2px;
-            padding: 0px 5px;
-            text-decoration: underline;
-        }
-        &:active {
-            margin: 1px 0px 0px 1px;
-            padding: 1px 5px 0px 6px;
-            box-shadow: 1px 1px var(--ctp-overlay0);
-            &:focus-visible {
-                padding: 1px 4px 0px 5px;
-            }
-        }
+    button:not(:disabled):hover {
+        background: var(--ctp-maroon);
+        color: var(--ctp-base);
+    }
+
+    button:not(:disabled):focus-visible {
+        border-color: var(--ctp-lavender);
+        border-width: 2px;
+        padding: 0px 5px;
+        text-decoration: underline;
+    }
+
+    button:not(:disabled):active {
+        margin: 1px 0px 0px 1px;
+        padding: 1px 5px 0px 6px;
+        box-shadow: 1px 1px var(--ctp-overlay0);
+    }
+
+    button:not(:disabled):active:focus-visible {
+        padding: 1px 4px 0px 5px;
     }
 
     button.safe {
         background: var(--ctp-sapphire);
-        &:hover {
-            background: var(--ctp-teal);
-        }
-        &:active {
-            border-right-color: var(--ctp-teal);
-            border-bottom-color: var(--ctp-teal);
-        }
+        color: var(--ctp-base);
+    }
+
+    button.safe:hover {
+        background: var(--ctp-teal);
+        color: var(--ctp-base);
+    }
+
+    button.safe:active {
+        border-right-color: var(--ctp-teal);
+        border-bottom-color: var(--ctp-teal);
     }
 
     button:disabled {
