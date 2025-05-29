040b9689db03a9c906efd80a372081e59b026af2
Hegui Dai <natural_selection_@outlook.com>
2025-05-29 20:58:19 +0800

    引入tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [svelte()],
+  plugins: [tailwindcss(), svelte()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
