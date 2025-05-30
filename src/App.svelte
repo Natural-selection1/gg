426c6cdf0d0fd767984778c4edfdfdb50915846c
Hegui Dai <natural_selection_@outlook.com>
2025-05-30 11:40:59 +0800

    npm run fmt

--- src/App.svelte
+++ src/App.svelte
@@ -91,7 +91,11 @@
     async function loadChange(id: RevId) {
         let rev = await query<RevResult>("query_revision", { id }, (q) => (selection = q));
 
-        if (rev.type == "data" && rev.value.type == "NotFound" && id.commit.hex != $repoStatusEvent?.working_copy.hex) {
+        if (
+            rev.type == "data" &&
+            rev.value.type == "NotFound" &&
+            id.commit.hex != $repoStatusEvent?.working_copy.hex
+        ) {
             return loadChange({
                 change: { type: "ChangeId", hex: "@", prefix: "@", rest: "" },
                 commit: $repoStatusEvent!.working_copy,
@@ -170,7 +174,9 @@
 </script>
 
 <Zone operand={{ type: "Repository" }} alwaysTarget let:target>
-    <div id="shell" class={$repoConfigEvent?.type == "Workspace" ? $repoConfigEvent.theme_override : ""}>
+    <div
+        id="shell"
+        class={$repoConfigEvent?.type == "Workspace" ? $repoConfigEvent.theme_override : ""}>
         {#if $repoConfigEvent.type == "Initial"}
             <Pane>
                 <h2 slot="header">Loading...</h2>
@@ -181,7 +187,9 @@
             <Pane />
         {:else if $repoConfigEvent.type == "Workspace"}
             {#key $repoConfigEvent.absolute_path}
-                <LogPane default_query={$repoConfigEvent.default_query} latest_query={$repoConfigEvent.latest_query} />
+                <LogPane
+                    default_query={$repoConfigEvent.default_query}
+                    latest_query={$repoConfigEvent.latest_query} />
             {/key}
 
             <div class="separator" />
@@ -193,7 +201,8 @@
                     <Pane>
                         <h2 slot="header">Not Found</h2>
                         <p slot="body">
-                            Revision <IdSpan id={data.id.change} />|<IdSpan id={data.id.commit} /> does not exist.
+                            Revision <IdSpan id={data.id.change} />|<IdSpan id={data.id.commit} /> does
+                            not exist.
                         </p>
                     </Pane>
                 {/if}
@@ -234,7 +243,10 @@
 
         {#if showOptionsDialog}
             <ModalOverlay>
-                <OptionsDialog {settings} on:save={handleSaveOptions} on:cancel={handleCancelOptions} />
+                <OptionsDialog
+                    {settings}
+                    on:save={handleSaveOptions}
+                    on:cancel={handleCancelOptions} />
             </ModalOverlay>
         {:else if $currentInput}
             <ModalOverlay>
@@ -247,7 +259,10 @@
         {:else if $currentMutation}
             <ModalOverlay>
                 {#if $currentMutation.type == "data" && ($currentMutation.value.type == "InternalError" || $currentMutation.value.type == "PreconditionError")}
-                    <ErrorDialog title="Command Error" onClose={() => ($currentMutation = null)} severe>
+                    <ErrorDialog
+                        title="Command Error"
+                        onClose={() => ($currentMutation = null)}
+                        severe>
                         {#if $currentMutation.value.type == "InternalError"}
                             <p>
                                 {#each $currentMutation.value.message.lines as line}
